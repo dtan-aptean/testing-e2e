020792b074da6b8a06814a649307653385890c68
Megan Kennemore <mkennemore@aptean.com>
2020-09-08 09:58:27 -0400

    Challenge dispute tests and rearranged Help Center tests to match other tests, plus sample documents for upload

--- src/merchant-portal/cypress/integration/help-section.spec.ts
+++ src/merchant-portal/cypress/integration/help-section.spec.ts
@@ -1,23 +1,23 @@
 /// <reference types="cypress" />
 
-describe('Help Center', function() {
-  context('Customer support', () => {
-    before(() => { 
-      sessionStorage.clear();
-      // navigate to home screen
-      cy.login();
-    });
+describe("Merchant Portal", function () {
+  before(() => {
+    sessionStorage.clear();
+    // navigate to home screen
+    cy.login();
+  });
+
+  context("Help Center", () => {
     beforeEach(() => {
-      cy.visit('/');
+      cy.visit("/");
       // navigate to help center screen
-      cy.get('[data-cy=user-settings]').click();
-      cy.get('[data-cy=help]').click();
+      cy.get("[data-cy=user-settings]").click();
+      cy.get("[data-cy=help]").click();
     });
 
-    it('should pass if displaying the product name or unknown', ()=> {
+    it("should pass if displaying the product name or unknown", () => {
       // Ensuring the product name is displaying, or at least unknown
-      cy.get('[data-cy=product-name]')
-        .should('be.visible');
+      cy.get("[data-cy=product-name]").should("be.visible");
     });
   });
 });
