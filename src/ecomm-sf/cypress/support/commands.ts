5b3d528f2bfddb03031607c2d5881c78c5ea04e9
Megan Kennemore <mkennemore@aptean.com>
2020-09-16 09:47:38 -0400

    Moved my tests into the ecomm-sf folder

--- src/ecomm-sf/cypress/support/commands.ts
+++ src/ecomm-sf/cypress/support/commands.ts
@@ -1,95 +1,170 @@
-Cypress.Commands.add('fromPublicStore_loginAsUser', (userProfile) => {  
-  cy.get('.ico-login').click()
+Cypress.Commands.add("fromPublicStore_loginAsUser", (userProfile) => {
+  cy.get(".ico-login").click();
 
-  cy.get('#Email')
+  cy.get("#Email")
     .type(userProfile.username)
-    .get('#Password')
+    .get("#Password")
     .type(userProfile.password)
-    .get('form > .buttons > .button-1')
-    .click()
-});
-
-Cypress.Commands.add('fromPublicStore_logout', () => {  
-  cy.get('.ico-logout').click()
-});
-
-Cypress.Commands.add('fromPublicStore_QuickCheckout', (profileName) => {  
-  cy.get('.ico-cart')
-    .should('be.visible')
+    .get("form > .buttons > .button-1")
     .click();
-
-  cy.get('#termsofservice')
-    .should('be.visible')
-    .check()    
-    .should('be.visible')
-    .get('#checkout')
-    .click()
 });
 
-Cypress.Commands.add('fromCheckoutAsGuest_FillForm', (profile) => {  
+Cypress.Commands.add("fromPublicStore_logout", () => {
+  cy.get(".ico-logout").click();
+});
 
+Cypress.Commands.add("fromPublicStore_QuickCheckout", (profileName) => {
+  cy.get(".ico-cart").should("be.visible").click();
+
+  cy.get("#termsofservice")
+    .should("be.visible")
+    .check()
+    .should("be.visible")
+    .get("#checkout")
+    .click();
+});
+
+Cypress.Commands.add("fromCheckoutAsGuest_FillForm", (profile) => {
   var contactDetails = [
     {
-      key: '#BillingNewAddress_FirstName',
-      value: profile.firstName
+      key: "#BillingNewAddress_FirstName",
+      value: profile.firstName,
     },
     {
-      key: '#BillingNewAddress_LastName',
-      value: profile.lastName
+      key: "#BillingNewAddress_LastName",
+      value: profile.lastName,
     },
     {
-      key: '#BillingNewAddress_Email',
-      value: profile.email
+      key: "#BillingNewAddress_Email",
+      value: profile.email,
     },
     {
-      key: '#BillingNewAddress_City',
-      value: profile.city
+      key: "#BillingNewAddress_City",
+      value: profile.city,
     },
     {
-      key: '#BillingNewAddress_Address1',
-      value: profile.address
+      key: "#BillingNewAddress_Address1",
+      value: profile.address,
     },
     {
-      key: '#BillingNewAddress_PhoneNumber',
-      value: profile.phone
+      key: "#BillingNewAddress_PhoneNumber",
+      value: profile.phone,
     },
     {
-      key: '#BillingNewAddress_ZipPostalCode',
-      value: profile.zipcode
-    }
-  ];
-  var dropDowns = [    
-    {
-      key: '#BillingNewAddress_CountryId',
-      value: profile.countryId
-    },
-    {
-      key: '#BillingNewAddress_StateProvinceId',
-      value: profile.stateId
+      key: "#BillingNewAddress_ZipPostalCode",
+      value: profile.zipcode,
     },
   ];
-  contactDetails.forEach(detail => {
-    cy.get(detail.key)
-    .type(detail.value)
-    .should('have.value', detail.value);
+  var dropDowns = [
+    {
+      key: "#BillingNewAddress_CountryId",
+      value: profile.countryId,
+    },
+    {
+      key: "#BillingNewAddress_StateProvinceId",
+      value: profile.stateId,
+    },
+  ];
+  contactDetails.forEach((detail) => {
+    cy.get(detail.key).type(detail.value).should("have.value", detail.value);
   });
-  dropDowns.forEach(detail => {
-    cy.get(detail.key)
-    .select(detail.value)
-    .should('have.value', detail.value);
+  dropDowns.forEach((detail) => {
+    cy.get(detail.key).select(detail.value).should("have.value", detail.value);
   });
-
 });
 
-Cypress.Commands.add('getIframeBody', (iFrameName) => {
+Cypress.Commands.add("getIframeBody", (iFrameName) => {
   // get the iframe > document > body
   // and retry until the body element is not empty
-  return cy
-    .get(iFrameName)
-    .its('0.contentDocument.body').should('not.be.empty')
-    // wraps "body" DOM element to allow
-    // chaining more Cypress commands, like ".find(...)"
-    // https://on.cypress.io/wrap
-    .then(cy.wrap);
+  return (
+    cy
+      .get(iFrameName)
+      .its("0.contentDocument.body")
+      .should("not.be.empty")
+      // wraps "body" DOM element to allow
+      // chaining more Cypress commands, like ".find(...)"
+      // https://on.cypress.io/wrap
+      .then(cy.wrap)
+  );
 });
 
+// Logs in with the configured username/password
+Cypress.Commands.add("login", () => {
+  Cypress.log({
+    name: "login",
+  });
+  cy.get(".header-links").find(".ico-login").click();
+  cy.wait(200);
+  cy.get(".email").type(Cypress.config("username"));
+  cy.get(".password").type(Cypress.config("password"));
+  cy.get(".login-button").click();
+});
+
+// Go to cart
+Cypress.Commands.add("goToCart", () => {
+  Cypress.log({
+    name: "goToCart",
+  });
+  cy.get(".header-links").find(".ico-cart").click();
+  cy.wait(500);
+});
+
+// Go to a catgory page. Will go to the Cypress Trees page unless another category is specified
+Cypress.Commands.add("goToCategory", (categoryName) => {
+  Cypress.log({
+    name: "goToCategory",
+    message: `${categoryName || "Cypress Trees"}`,
+    consoleProps: () => {
+      return {
+        "Category Name": categoryName,
+      };
+    },
+  });
+  cy.get(".top-menu.notmobile").then(($el) => {
+    if ($el.css("display") === "none") {
+      cy.get(".menu-toggle").click();
+      cy.wait(500);
+      cy.get(".top-menu.mobile")
+        .find("li")
+        .contains(categoryName || "Cypress Trees")
+        .click();
+    } else {
+      cy.wrap($el)
+        .find("li")
+        .contains(categoryName || "Cypress Trees")
+        .click();
+    }
+    cy.wait(500);
+  });
+});
+
+/**
+ * Goes to a specific product page.
+ * Assumes that the product is under the Cypress Trees category,
+ * unless a category name is specified
+ */
+Cypress.Commands.add("goToProduct", (productName, categoryName) => {
+  Cypress.log({
+    name: "goToProduct",
+    message: `${categoryName ? categoryName + ", " : ""}${productName}`,
+    consoleProps: () => {
+      return {
+        "Category Name": categoryName || "Not provided",
+        "Product Name": productName,
+      };
+    },
+  });
+  cy.goToCategory(categoryName || "Cypress Trees");
+
+  cy.get(".item-box")
+    .filter((index, item) => {
+      return item.innerText.includes(productName);
+    })
+    .as("targetProduct");
+  cy.get("@targetProduct")
+    .find(".details")
+    .scrollIntoView()
+    .should("be.visible");
+  cy.get("@targetProduct").find(".product-title").find("a").click();
+  cy.wait(500);
+});
