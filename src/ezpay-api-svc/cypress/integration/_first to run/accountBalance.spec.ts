133f3e9c84a1b27cde7b58521967dcc9ecf63ade
Ferry Jongmans <ferryjongmans@live.nl>
2020-12-24 15:33:45 +0100

    Account balance tests

---
+++ src/ezpay-api-svc/cypress/integration/_first to run/accountBalance.spec.ts
@@ -0,0 +1,66 @@
+/// <reference types="cypress" />
+// @ts-check
+
+describe("Query: AccountBalance", () => {
+  it("should pass if the wepay balance is the same as in our tenant account", () => {
+    cy.getWePayAccount(Cypress.env("wepay-account-id")).then((res) => {
+      const {
+        balance,
+        incoming_pending,
+        outgoing_pending,
+        reserve,
+      } = res.balances.currencies.USD;
+
+      const gqlQuery = `
+      query {
+        account {
+          balances{
+            currentBalance {
+              amount
+              currency
+            }
+            incomingPendingBalance {
+              amount
+              currency
+            }
+            outgoingPendingBalance {
+              amount
+              currency
+            }
+            currentReserve {
+              amount
+              currency
+            }
+          }
+        }
+      }`;
+
+      cy.postGQL(gqlQuery).then((res) => {
+        const {
+          currentBalance,
+          currentReserve,
+          incomingPendingBalance,
+          outgoingPendingBalance,
+        } = res.body.data.account.balances;
+
+        // should be 200 ok
+        cy.expect(res.isOkStatusCode).to.be.equal(true);
+
+        // no errors
+        assert.notExists(
+          res.body.errors,
+          `One or more errors ocuured while executing query: ${gqlQuery}`
+        );
+
+        // should be equal to all balances retrieved from wepay
+        assert.equal(currentBalance.amount, balance);
+        assert.equal(incomingPendingBalance.amount, incoming_pending);
+        assert.equal(outgoingPendingBalance.amount, outgoing_pending);
+        assert.equal(currentReserve.amount, reserve);
+
+        // has data
+        assert.exists(res.body.data);
+      });
+    });
+  });
+});
