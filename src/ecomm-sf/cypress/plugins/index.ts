1cd9b1027911b78c5736eaa4fc488bdf040e9f9d
David Tan <qing.tan@aptean.com>
2021-06-07 09:52:54 -0400

    Remove debug info

--- src/ecomm-sf/cypress/plugins/index.ts
+++ src/ecomm-sf/cypress/plugins/index.ts
@@ -30,7 +30,6 @@ module.exports = (on, config) => {
           "value": e.state
         }
       });
-      console.log(JSON.stringify(facts));
       await axios.post("https://apteanonline.webhook.office.com/webhookb2/b879817b-ffa3-404c-8f59-37ecabce0a54@560ec2b0-df0c-4e8c-9848-a15718863bb6/IncomingWebhook/eb364835301e4e84a8228da33ba09146/a7c163f2-fcf7-4365-a94c-6d9bf23155cc", {      
           "@type": "MessageCard",
           "@context": "http://schema.org/extensions",
