768ac644e1161a1ad214a9a6f91994f4dd9aa334
Qing Tan <david.tan@aptean.com>
2021-06-22 18:17:09 +0000

    Merged PR 27468: Generate HTML report and upload to blob storage
    Generate HTML report and upload to blob storage
    
    Related work items: #228735

--- src/ezpay-payer-portal/cypress/plugins/index.ts
+++ src/ezpay-payer-portal/cypress/plugins/index.ts
@@ -12,6 +12,9 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 import axios from 'axios';
+import * as fs from 'fs';
+import * as mime from 'mime-types';
+import { BlobServiceClient } from "@azure/storage-blob";
 import * as helper from '../support/getAuthToken';
 const { beforeRunHook, afterRunHook } = require('cypress-mochawesome-reporter/lib');
 /**
@@ -20,9 +23,10 @@ const { beforeRunHook, afterRunHook } = require('cypress-mochawesome-reporter/li
 module.exports = async (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
-  const dateSubFolder = new Date().toISOString().substring(0,10);
+  const dateSubFolder = new Date().toISOString().substring(0,19);
+  const storageConnString = config.env.storageAccountConnString;
   config.screenshotsFolder = `${config.screenshotsFolder}/${dateSubFolder}`;
-  config.reporterOptions.reportDir = `/e2e/cypress/results/${dateSubFolder}`;  
+  config.reporterOptions.reportDir = `${config.reporterOptions.reportDir}/${dateSubFolder}`
 
   const token = await helper.getAdToken(config.env.username, config.env.password, config.env.tokenUrl);
   config.env.authorization = token;  
@@ -36,6 +40,13 @@ module.exports = async (on, config) => {
     console.log('override after:run');
     await afterRunHook();
 
+    if (config.env.runtimeEnv == 'local') 
+      return;
+
+    const blobServiceClient = BlobServiceClient.fromConnectionString(storageConnString);
+    const reportContainer = blobServiceClient.getContainerClient("ezpay-payer-portal-cypress-report");
+    await uploadBlobs(reportContainer, results.config.reporterOptions.reportDir, dateSubFolder);    
+    
     if (results) {
       let facts: Array<any> = [];
       facts.push({
@@ -66,7 +77,7 @@ module.exports = async (on, config) => {
               "name": "See Details",
               "targets": [{
                   "os": "default",
-                  "uri": "https://portal.azure.com/#blade/Microsoft_Azure_FileStorage/FileShareMenuBlade/overview/storageAccountId/%2Fsubscriptions%2Fa31596c1-e218-48d6-ad65-c7beafeb2bfa%2FresourceGroups%2Frg-eastus-tst-ecommerce%2Fproviders%2FMicrosoft.Storage%2FstorageAccounts%2Fstecommercetenanttst001/path/cypress-ezpay-payer-portal/protocol/SMB"
+                  "uri": `https://stecommercetenanttst001.blob.core.windows.net/ezpay-payer-portal-cypress-report/${dateSubFolder}/index.html`
               }]
           }]
       });
@@ -74,3 +85,60 @@ module.exports = async (on, config) => {
   });
   return config;   
 };
+
+const walk = (dir, done) => {
+  let results:any[] = [];
+  let i = 0;
+  const list = fs.readdirSync(dir);
+  (function next() {
+    let file = list[i++];
+    if (!file) {
+      return done(null, results);
+    }
+    file = dir + '/' + file;
+    const fsStat = fs.statSync(file);
+    if (fsStat && fsStat.isDirectory()) {
+      walk(file, (err, res) => {
+        results = results.concat(res);
+        next();
+      });
+    } else {
+      results.push(file);
+      next();
+    }    
+  })();
+};
+
+const uploadBlobs = async (containerClent, sourceDirectoryPath, dateSubFolder) => {
+  if (!fs.existsSync(sourceDirectoryPath)) {
+    console.log(sourceDirectoryPath + ' is an invalid directory path.');
+    return;
+  }
+  let fileList:any[] = [];
+  // Search the directory and generate a list of files to upload.
+  walk(sourceDirectoryPath, function (error, files) {
+    if (error) {
+      console.log(error);
+    } else {
+      fileList = files;
+    }
+  });
+
+  for (const file of fileList) {
+    console.log(`Processing file ${file}`);
+    const blobName = file.substr(sourceDirectoryPath.length + 1);
+    const blobClient = containerClent.getBlockBlobClient(`${dateSubFolder}/${blobName}`);
+    try {
+      await blobClient.uploadFile(file, {
+        blobHTTPHeaders: {
+          blobContentType: mime.lookup(file)
+        },
+        blockSize: 4 * 1024 * 1024,
+        concurrency: 20
+      });
+      console.log(`Upload file ${file} succeeds`);
+    } catch (err) {
+      console.log(`Upload file failed, requestId - ${err.details.requestId}, statusCode - ${err.statusCode}, errorCode - ${err.details.errorCode}`);
+    }    
+  };  
+}
