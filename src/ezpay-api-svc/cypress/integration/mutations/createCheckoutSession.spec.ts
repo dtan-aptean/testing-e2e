5b0750da18d6cee89622c2d7a2156ffe7b37f7a2
Ferry Jongmans <fjongmans@aptean.com>
2021-07-06 16:54:25 +0000

    Merged PR 28430: Check for address line 298
    Check for address line 298
    
    Related work items: #232176

--- src/ezpay-api-svc/cypress/integration/mutations/createCheckoutSession.spec.ts
+++ src/ezpay-api-svc/cypress/integration/mutations/createCheckoutSession.spec.ts
@@ -1,8 +1,8 @@
 /// <reference types="cypress" />
 
 describe("Mutation: checkout session", () => {
-    it("should pass if the session is created", () => {
-      const gqlQuery = `mutation {
+  it("should pass if the session is created", () => {
+    const gqlQuery = `mutation {
         createCheckoutSession(
           input: {
             amount: 3500
@@ -41,24 +41,24 @@ describe("Mutation: checkout session", () => {
         }
       }      
       `;
-  
-      cy.postGQLCheckoutConsumer(gqlQuery).then((res) => {
-        // should be 200 ok
-        cy.expect(res.isOkStatusCode).to.be.equal(true);
-  
-        // no errors
-        assert.notExists(res.body.errors, 'no errors');
-  
-        // has data
-        assert.exists(res.body.data);
 
-        // return type should not be nulll
-        assert.isNotNull(res.body.data.createCheckoutSession.checkoutSession.id);
-      });
+    cy.postGQLCheckoutConsumer(gqlQuery).then((res) => {
+      // should be 200 ok
+      cy.expect(res.isOkStatusCode).to.be.equal(true);
+
+      // no errors
+      assert.notExists(res.body.errors, "no errors");
+
+      // has data
+      assert.exists(res.body.data);
+
+      // return type should not be nulll
+      assert.isNotNull(res.body.data.createCheckoutSession.checkoutSession.id);
     });
-  
-    it("should fail if the amount is less than $1", () => {
-        const gqlQuery = `mutation {
+  });
+
+  it("should fail if the amount is less than $1", () => {
+    const gqlQuery = `mutation {
             createCheckoutSession(
               input: {
                 amount: 0.1
@@ -97,18 +97,18 @@ describe("Mutation: checkout session", () => {
             }
           }      
           `;
-  
-      cy.postGQLCheckoutConsumer(gqlQuery).then((res) => {
-        // should have errors
-        assert.exists(res.body.errors);
-  
-        // no data
-        assert.notExists(res.body.data);
-      });
+
+    cy.postGQLCheckoutConsumer(gqlQuery).then((res) => {
+      // should have errors
+      assert.exists(res.body.errors);
+
+      // no data
+      assert.notExists(res.body.data);
     });
-  
-    it("should fail if input argument is empty", () => {
-      const gqlQuery = `mutation {
+  });
+
+  it("should fail if input argument is empty", () => {
+    const gqlQuery = `mutation {
         createCheckoutSession(
           input: {
           }
@@ -120,18 +120,18 @@ describe("Mutation: checkout session", () => {
       }
       
       `;
-  
-      cy.postGQLCheckoutConsumer(gqlQuery).then((res) => {
-        // should have errors
-        assert.exists(res.body.errors);
-  
-        // no data
-        assert.notExists(res.body.data);
-      });
-    });
 
-    it("should fail if no return type is provided", () => {
-        const gqlQuery = `mutation {
+    cy.postGQLCheckoutConsumer(gqlQuery).then((res) => {
+      // should have errors
+      assert.exists(res.body.errors);
+
+      // no data
+      assert.notExists(res.body.data);
+    });
+  });
+
+  it("should fail if no return type is provided", () => {
+    const gqlQuery = `mutation {
             createCheckoutSession(
               input: {
                 amount: 1200
@@ -165,18 +165,18 @@ describe("Mutation: checkout session", () => {
               }
             )
           }`;
-    
-        cy.postGQLCheckoutConsumer(gqlQuery).then((res) => {
-          // should have errors
-          assert.exists(res.body.errors);
-    
-          // no data
-          assert.notExists(res.body.data);
-        });
-      });
 
-      it("should pass creating a checkout session without address", () => {
-        const gqlQuery = `mutation {
+    cy.postGQLCheckoutConsumer(gqlQuery).then((res) => {
+      // should have errors
+      assert.exists(res.body.errors);
+
+      // no data
+      assert.notExists(res.body.data);
+    });
+  });
+
+  it("should pass creating a checkout session without address", () => {
+    const gqlQuery = `mutation {
             createCheckoutSession(
               input: {
                 amount: 1200
@@ -222,24 +222,26 @@ describe("Mutation: checkout session", () => {
             }
           }
           `;
-    
-        cy.postGQLCheckoutConsumer(gqlQuery).then((res) => {
-            // should be 200 ok
-            cy.expect(res.isOkStatusCode).to.be.equal(true);
-    
-            // no errors
-            assert.notExists(res.body.errors, 'no errors');
-    
-            // has data
-            assert.exists(res.body.data);
 
-            // if address is not specified then in result it will be null
-            assert.isNull(res.body.data.createCheckoutSession.checkoutSession.payerDetails.address);
-        });
-      });
+    cy.postGQLCheckoutConsumer(gqlQuery).then((res) => {
+      // should be 200 ok
+      cy.expect(res.isOkStatusCode).to.be.equal(true);
 
-      it("should pass creating a checkout session without payer-details", () => {
-        const gqlQuery = `mutation {
+      // no errors
+      assert.notExists(res.body.errors, "no errors");
+
+      // has data
+      assert.exists(res.body.data);
+
+      // if address is not specified then in result it will be null
+      assert.isNull(
+        res.body.data.createCheckoutSession.checkoutSession.payerDetails.address
+      );
+    });
+  });
+
+  it("should pass creating a checkout session without payer-details", () => {
+    const gqlQuery = `mutation {
             createCheckoutSession(
               input: {
                 amount: 1200
@@ -280,24 +282,26 @@ describe("Mutation: checkout session", () => {
             }
           }
           `;
-    
-        cy.postGQLCheckoutConsumer(gqlQuery).then((res) => {
-            // should be 200 ok
-            cy.expect(res.isOkStatusCode).to.be.equal(true);
-    
-            // no errors
-            assert.notExists(res.body.errors, 'no errors');
-    
-            // has data
-            assert.exists(res.body.data);
 
-            // if address is not specified then in result it will be null
-            assert.isNull(res.body.data.createCheckoutSession.checkoutSession.payerDetails);
-        });
-      });
+    cy.postGQLCheckoutConsumer(gqlQuery).then((res) => {
+      // should be 200 ok
+      cy.expect(res.isOkStatusCode).to.be.equal(true);
 
-      it("should fail when creating a checkout session with invalid country in payer details", () => {
-        const gqlQuery = `mutation {
+      // no errors
+      assert.notExists(res.body.errors, "no errors");
+
+      // has data
+      assert.exists(res.body.data);
+
+      // if address is not specified then in result it will be null
+      assert.isNull(
+        res.body.data.createCheckoutSession.checkoutSession.payerDetails.address
+      );
+    });
+  });
+
+  it("should fail when creating a checkout session with invalid country in payer details", () => {
+    const gqlQuery = `mutation {
             createCheckoutSession(
               input: {
                 amount: 1200
@@ -331,18 +335,18 @@ describe("Mutation: checkout session", () => {
               }
             )
           }`;
-    
-        cy.postGQLCheckoutConsumer(gqlQuery).then((res) => {
-          // should have errors
-          assert.exists(res.body.errors);
-    
-          // no data
-          assert.notExists(res.body.data);
-        });
-      });
 
-      it("should pass creating a checkout session without phone", () => {
-        const gqlQuery = `mutation {
+    cy.postGQLCheckoutConsumer(gqlQuery).then((res) => {
+      // should have errors
+      assert.exists(res.body.errors);
+
+      // no data
+      assert.notExists(res.body.data);
+    });
+  });
+
+  it("should pass creating a checkout session without phone", () => {
+    const gqlQuery = `mutation {
             createCheckoutSession(
               input: {
                 amount: 1200
@@ -392,19 +396,21 @@ describe("Mutation: checkout session", () => {
             }
           }
           `;
-    
-        cy.postGQLCheckoutConsumer(gqlQuery).then((res) => {
-            // should be 200 ok
-            cy.expect(res.isOkStatusCode).to.be.equal(true);
-    
-            // no errors
-            assert.notExists(res.body.errors, 'no errors');
-    
-            // has data
-            assert.exists(res.body.data);
 
-            // if address is not specified then in result it will be null
-            assert.isNull(res.body.data.createCheckoutSession.checkoutSession.payerDetails.phone);
-        });
-      });
+    cy.postGQLCheckoutConsumer(gqlQuery).then((res) => {
+      // should be 200 ok
+      cy.expect(res.isOkStatusCode).to.be.equal(true);
+
+      // no errors
+      assert.notExists(res.body.errors, "no errors");
+
+      // has data
+      assert.exists(res.body.data);
+
+      // if address is not specified then in result it will be null
+      assert.isNull(
+        res.body.data.createCheckoutSession.checkoutSession.payerDetails.phone
+      );
+    });
   });
+});
