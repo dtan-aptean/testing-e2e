bc64b2c72119e44b7f31a717aa108d0e622512b3
azhamkochyan <aram.zhamkochyan@dataart.com>
2021-07-07 20:30:49 +0400

    a test for not processing completed session

--- src/ezpay-checkout-svc/cypress/support/commands.ts
+++ src/ezpay-checkout-svc/cypress/support/commands.ts
@@ -85,3 +85,15 @@ Cypress.Commands.add("createCheckoutSession", () => {
  `;
  return cy.postGQLCheckoutConsumer(mutation);
 });
+
+Cypress.Commands.add('getIframeBody', (iframeSelector) => {
+    // get the iframe > document > body
+    // and retry until the body element is not empty
+    return cy
+    .get(iframeSelector)
+    .its('0.contentDocument.body').should('not.be.empty')
+    // wraps "body" DOM element to allow
+    // chaining more Cypress commands, like ".find(...)"
+    // https://on.cypress.io/wrap
+    .then(cy.wrap)
+});
