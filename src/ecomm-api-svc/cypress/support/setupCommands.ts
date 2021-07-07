d0ea31c23f28c50ad3867695b5d79fdad413761e
Poornima Dhayalamoorthy <Poornima.Dhayalamoorthy@aptean.com>
2021-07-07 16:40:58 +0530

    Merged PR 27305: Html report in all cypress tests
    Html report in all cypress tests
    
    Related work items: #228735

--- src/ecomm-api-svc/cypress/support/setupCommands.ts
+++ src/ecomm-api-svc/cypress/support/setupCommands.ts
@@ -50,18 +50,27 @@ const performDelete = (deleteName: string, id: string, altUrl?: string) => {
 };
 
 // Make sure each item is a Cypress item, and if it is, call command to delete it
-const deleteItems = (nodes, deleteName: string, searchBy: string, infoName?: string, altUrl?: string) => {
+const deleteItems = (nodes, deleteName: string, searchBy: string, infoName?: string, altUrl?: string, descendingName?:string) => {
     nodes.forEach((item) => {
         var id = item.id;
         if (infoName) {
             const nameArray = item[infoName].filter((nameItem) => {
-				return nameItem.name.includes(searchBy) && nameItem.languageCode === "Standard";
+                return nameItem.name.includes(searchBy) && nameItem.languageCode === "Standard";
             });
             if (nameArray.length > 0) {
                 performDelete(deleteName, id, altUrl);
             }
         } else if (deleteName === "deletePaymentSettings") {
             var name = item.company.name;
+            if (name.includes(searchBy)) {
+                performDelete(deleteName, id, altUrl);
+            }
+        } else if (descendingName) {
+            var levels = descendingName.split(".");
+            var name = item;
+            levels.forEach((nam) => {
+                name = name[nam];
+            });
             if (name.includes(searchBy)) {
                 performDelete(deleteName, id, altUrl);
             }
@@ -74,13 +83,16 @@ const deleteItems = (nodes, deleteName: string, searchBy: string, infoName?: str
     });
 };
 
-const getNameField = (infoName?: string): string => {
+const getNameField = (infoName?: string,queryName?:String): string => {
     var nameField = "name";
     if (infoName) {
         nameField = `${infoName} {
             name
             languageCode
         }`;
+    }
+    if(queryName == "customers"){
+        nameField = "firstName"
     }
     return nameField;
 };
@@ -92,10 +104,11 @@ const getNodes = (
     searchBy: string,
     infoName?: string,
     additionalFields?: string,
-    altUrl?: string
+    altUrl?: string,
+    additionalInput?: string
 ) => {
-    const nameField = queryName === "paymentSettings" ? "" : getNameField(infoName);
-    const queryBody = `searchString: "${searchBy}", orderBy: {direction: ASC, field: ${queryName === "paymentSettings" ? "COMPANY_NAME" : "NAME"}}) {
+    const nameField = (queryName === "paymentSettings" || queryName === "addresses") ? "" : getNameField(infoName, queryName);
+    const queryBody = `${additionalInput ? additionalInput + ", " : ""}searchString: "${searchBy}", orderBy: {direction: ASC, field: ${queryName === "paymentSettings" ? "COMPANY_NAME" : "NAME"}}) {
         totalCount
         nodes {
             id
@@ -104,7 +117,7 @@ const getNodes = (
         }
     }`;
     const query = `{
-		${queryName}(${queryBody}
+        ${queryName}(${queryBody}
     }`;
     return cy.postNoFail(query, queryName, altUrl).then((res) => {
         if (res) {
@@ -367,3 +380,72 @@ Cypress.Commands.add("setupRequiredProducts", () => {
         });
     });
 });
+
+Cypress.Commands.add("queryCompanyCustomerWithAddresses", (queryName: string, searchString?: string, altUrl?:string ) => {
+    const searchBy = searchString ? searchString : "Cypress Address";
+    var ids = [];
+        getNodes(queryName, searchBy, undefined, undefined, altUrl).then((nodes) => {
+            cy.log(nodes)
+    if(nodes){
+        if (nodes.length > 0) {
+            nodes.forEach((node) => {
+                    ids.push(node.id)
+                });  
+            }  
+        }
+        cy.log(ids)
+        cy.wrap(ids);
+    });
+});
+
+// Command to delete items that require special search input
+Cypress.Commands.add("deleteSpecialCypressItems", (
+    queryName: string,
+    deleteName: string,
+    specialInput: string,   // EX: a companyId or customerId
+    specialInputName: string,   // What the special input is: if it's a companyId, pass in 'companyId'
+    searchString?: string,
+    altUrl?: string
+) => {
+    const searchBy = searchString ? searchString : "Cypress";
+    var extraField;
+    if (specialInputName === "customerId") {
+        extraField = `customer {
+            id
+            email
+            firstName
+            lastName
+        }`
+    } else if (specialInputName === "companyId") {
+        extraField = `company {
+            id
+            name
+        }`
+    }
+    Cypress.log({name: "deleteSpecialCypressItems", message: `Using ${queryName} to search for "${searchBy}"`});
+    const input = typeof specialInput === "string" ? `${specialInputName}: "${specialInput}"` : `${specialInputName}: ${specialInput}`;
+    getNodes(queryName, searchBy, undefined, extraField, altUrl, input).then((nodes) => {
+        if (nodes) {
+            if (specialInputName === "customerId") {
+                const validNodes = nodes.filter((node) => {
+                    return node.customer.email.toLowerCase().includes("cypress") || node.customer.firstName.toLowerCase().includes("cypress") || node.customer.lastName.toLowerCase().includes("cypress");
+                });
+                if (validNodes.length > 0) {
+                    deleteItems(validNodes, deleteName, searchBy, undefined, altUrl, "customer.email");
+                } else {
+                    Cypress.log({message: "No Cypress items found"});
+                }
+            } else if (specialInputName === "companyId") {
+                const validNodes = nodes.filter((node) => {
+                    return node.company.name.toLowerCase().includes("cypress");
+                });
+                if (validNodes.length > 0) {
+                    deleteItems(validNodes, deleteName, searchBy, undefined, altUrl, "company.name");
+                } else {
+                    Cypress.log({message: "No Cypress items found"});
+                }
+            }
+        }
+    });
+});
+
